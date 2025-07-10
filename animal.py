60c1745feb68e911006e0a9ce370b3866e23e946
Bhavy0309 <dbhavy03@gmail.com>
2025-07-10 19:04:04 +0530

    first commit

--- animal.py
+++ animal.py
@@ -1,16 +1,20 @@
 animal=input("what is your favorite animal ")
-dog=input
-if input==dog:
+dog= "dog"
+cat='cat'
+lion= "lion"
+if dog == "dog":
     print("Dogs are loyal and asesome!")
-else:
-    print("that's a cool animal!")
 
-cat=input
-
-if cat==input:
+elif cat == "cat":
     print("cats are cute and love naps.")
-lion=input
-if lion==input:
+elif lion == "lion":
     print("lions are the kings of the woods!")
+else :
+    print("cool animal")
+### elif cat==input:
+###print("cats are cute and love naps.")
+### lion=input
+### if lion==input:
+    ##print("lions are the kings of the woods!")
 
     
