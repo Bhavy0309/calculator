00312a99721f3e829374eb7be47b8d498cf15476
Bhavy0309 <dbhavy03@gmail.com>
2025-07-10 19:16:42 +0530

    second commit

--- calculator.js
+++ calculator.js
@@ -1,1 +1,3 @@
 alert("greetings user")
+alert("this is a simple calculator which is currently in work in progress")
+alert("You can go to https://calculate-helper.netlify.app to see the completed project(calculator)")
